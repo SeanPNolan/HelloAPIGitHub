d0a1345600d13c3e09604d6390227b587caaa3fc
SeanPNolan <90008006+SeanPNolan@users.noreply.github.com>
2022-03-16 10:01:52 -0400

    First Commit

---
+++ src/main/java/com/example/HelloAPI/Controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.HelloAPI.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Sean Nolan was here!";
+    }
+}
